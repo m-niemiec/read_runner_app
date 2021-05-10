5ab60324e4fcab5d2b847e021edc0682f0f7bdfc
mniemiec <mniemiec@appriss.com>
2021-05-10 22:15:00 +0200

    Importing and parsing file work in progress

--- importtext.py
+++ importtext.py
@@ -2,6 +2,11 @@ import re
 import sqlite3
 import uuid
 import pdfplumber
+from functools import partial
+import ebooklib
+from ebooklib import epub
+import mobi
+import html2text
 
 from kivy.core.clipboard import Clipboard
 from kivy.properties import StringProperty
@@ -14,6 +19,11 @@ from kivy.metrics import dp
 from kivy.properties import Clock
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
+from concurrent.futures import ThreadPoolExecutor
+
+
+class TextLoading(Screen):
+    import_progress = StringProperty('Estimating PDF size ...')
 
 
 class ImportText(Screen):
@@ -24,6 +34,9 @@ class ImportText(Screen):
     warning_dialog = None
     text_loading_dialog = None
     new_text = None
+    text_loading = None
+    pdf_page_count = 0
+    import_progress = StringProperty('0%')
 
     def import_from_clipboard(self):
         self.imported_text = Clipboard.paste()
@@ -44,12 +57,13 @@ class ImportText(Screen):
         self.manager_open = True
 
     def select_path(self, path):
-        self.exit_manager()
-        import time
-        time.sleep(1)
         text_file_path = path
         toast(text_file_path)
-        self.determine_file_type(text_file_path)
+        self.text_loading()
+        self.exit_manager()
+        executor = ThreadPoolExecutor(5)
+        executor.submit(partial(self.determine_file_type, text_file_path))
+        # self.determine_file_type(text_file_path)
 
     def exit_manager(self, *args):
         self.manager_open = False
@@ -62,6 +76,8 @@ class ImportText(Screen):
             self.import_pdf_file(text_file_path)
         elif re.search(r'\S+.mobi|\S+.MOBI', text_file_path):
             self.import_mobi_file(text_file_path)
+        elif re.search(r'\S+.epub|\S+.EPUB', text_file_path):
+            self.import_epub_file(text_file_path)
         else:
             pass
             # show dialog about wrong file type
@@ -70,50 +86,62 @@ class ImportText(Screen):
         with open(text_file_path, encoding='utf-8-sig') as file:
             new_text = file.readlines()
 
-        MDApp.get_running_app().root.get_screen("importtext").ids.imported_text_field.text = new_text
+        MDApp.get_running_app().root.get_screen("importtext").ids.imported_text_field.text = new_text[:1000]
 
-    def import_pdf_file(self, text_file_path):
-
-        self.text_loading()
+    def import_pdf_file(self, text_file_path, obj=None):
+        pdf_page_count = len(pdfplumber.open(text_file_path).pages)
 
         i = 0
-        new_text = ''
 
         with pdfplumber.open(text_file_path) as pdf:
-            for page in pdf.pages[:10]:
+            for page in pdf.pages[:50]:
                 print(i)
                 i += 1
 
-                new_text += str(page.extract_text()).replace('\n', ' ')
+                import_progress = '{:.2f}'.format(i / pdf_page_count * 100)
 
-                # connection = sqlite3.connect('read_runner.db')
-                # cursor = connection.cursor()
-                # cursor.execute("INSERT INTO temp_text VALUES (?, ?)",
-                #                (text_file_path, str(page.extract_text()).replace('\n', ' ')))
+                self.text_loading.import_progress = f'Imported - {import_progress}%'
 
-                # cursor.execute("UPDATE temp_text SET text_body = text_body + ? WHERE text_id = ",
-                #                (text_file_path, str(page.extract_text()).replace('\n', ' ')))
+                new_page = str(page.extract_text()).replace('\n', ' ')
 
-                # test = str(page.extract_text()).replace('\n', ' ')
-
-                # cursor.execute(f"UPDATE temp_text SET text_body = text_body + (?)", (test, ))
-                #
-                # connection.commit()
+                self.save_temp_data(new_page)
 
                 del page._objects
                 del page._layout
+                # page.flush_cache()
 
-        self.new_text = new_text
+        # MDApp.get_running_app().root.get_screen("importtext").ids.imported_text_field.text = new_text[:1000]
 
-        MDApp.get_running_app().root.get_screen("importtext").ids.imported_text_field.text = new_text[:100]
-
-        # self.text_loading_dialog.dismiss()
+        self.text_loading_dialog.dismiss()
 
     def import_mobi_file(self, text_file_path):
-        pass
+        tempdir, filepath = mobi.extract(text_file_path)
+        file = open(filepath, 'r', errors='ignore')
+        content = file.read()
+        return html2text.html2text(content.replace('\\n', ''))
 
-    # def select_text_type(self, text_type):
-    #     print(text_type)
+    def import_epub_file(self, text_file_path):
+        book = epub.read_epub(text_file_path)
+        chapters = []
+        for item in book.get_items():
+            if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                # chapters.append(item.get_content())
+                self.save_temp_data(str(item.get_content()).replace('\\n', ''))
+
+        # return html2text.html2text(str(chapters)).replace('\\n', '')
+
+    def save_temp_data(self, text_body):
+        connection = sqlite3.connect('read_runner.db')
+        cursor = connection.cursor()
+
+        # cursor.execute('DROP TABLE IF EXISTS temp_data')
+        cursor.execute('CREATE TABLE IF NOT EXISTS temp_data (temp_text_body text)')
+        # cursor.execute('INSERT INTO temp_data VALUES (?)',
+        #                (text_body, ))
+        cursor.execute('UPDATE temp_data SET "temp_text_body" = "temp_text_body" || (?)',
+                       (text_body, ))
+        print(text_body[:10])
+        connection.commit()
 
     def save_new_text(self):
         text_title = MDApp.get_running_app().root.get_screen("importtext").ids.imported_text_title_field.text
@@ -164,9 +192,13 @@ class ImportText(Screen):
         self.warning_dialog.dismiss()
 
     def text_loading(self):
-        print("im here")
-        MDApp.get_running_app().root.get_screen("readtext").manager.transition.direction = 'right'
-        MDApp.get_running_app().root.get_screen("readtext").manager.current = 'mainscreen'
+        self.text_loading = TextLoading()
 
-    def text_loading_close(self):
-        self.text_loading_dialog.dismiss()
+        self.text_loading_dialog = MDDialog(
+            title='Please wait ...',
+            type='custom',
+            content_cls=self.text_loading,
+            auto_dismiss=False)
+
+        self.text_loading_dialog.open()
+

2bcad0d05076f64c673c043fd076d5ccdcf58eb2
mniemiec <mniemiec@appriss.com>
2021-05-15 22:05:49 +0200

    More work on frontend part of the app

--- helper_texts.py
+++ helper_texts.py
@@ -6,9 +6,7 @@ Here is a quick guide:
  - To import book/text press plus icon in bottom right corner.
 There you will be able to either paste copied text or select file to import.
 
- - Accepted formats are .TXT, .PDF and .MOBI
-
- - Bottom left icon (book) will get you back to your books list.
+ - Accepted formats are .TXT, .PDF, .MOBI and .EPUB
 
  - In top left corner there is a Menu button where you will find more help and options.
 
